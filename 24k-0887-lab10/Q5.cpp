a7b1639a9dff5e65e14b3702493c658aa18f8f41
Muzammil Zaidi <muzammil.s3517@gmail.com>
2025-11-20 15:07:52 +0500

    Create Q5.cpp

---
+++ 24k-0887-lab10/Q5.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+
+class MaxHeap{
+public:
+    int arr[100];
+    int size;
+
+    MaxHeap(){
+        size=0;
+    }
+
+    void insert(int value){
+        arr[++size]=value;
+        int i=size;
+
+        while(i>1 && arr[i]>arr[i/2]){
+            swap(arr[i],arr[i/2]);
+            i=i/2;
+        }
+    }
+
+    void removeMax(){
+        if(size==0){
+            return;
+        }
+
+        arr[1]=arr[size];
+        size--;
+
+        int i=1;
+        while(true){
+            int left=2*i;
+            int right=2*i+1;
+            int largest=i;
+
+            if(left<=size && arr[left]>arr[largest]){
+                largest=left;
+            }
+            
+            if(right<=size && arr[right]>arr[largest]){
+                largest=right;
+            }
+
+            if(largest==i){
+                break;
+            }
+
+            swap(arr[i],arr[largest]);
+            i=largest;
+        }
+    }
+
+    void display(){
+        for(int i=1;i<=size;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
+
+int main(){
+    MaxHeap h;
+
+    h.insert(1500);
+    h.insert(1200);
+    h.insert(1800);
+    h.insert(1600);
+    cout<<"Initial queue: ";
+    h.display();
+
+    h.insert(1700);
+    cout<<"Queue after inserting 1700: ";
+    h.display();
+
+    h.removeMax();
+    cout<<"Queue after removing highest rating: ";
+    h.display();
+
+    return 0;
+}
